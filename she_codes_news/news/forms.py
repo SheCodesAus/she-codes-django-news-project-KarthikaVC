f13e81119b95e5ed4cabe0fd1f7ad92098bf46b7
KarthikaVC <karthikav.rmkcse@gmail.com>
2022-12-14 05:53:46 +0530

    Setup and basic Changes

---
+++ she_codes_news/news/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import ModelForm
+from .models import NewsStory
+
+# Added by Karthika V
+import datetime
+
+
+class StoryForm(ModelForm):
+    class Meta:
+        model = NewsStory
+        fields = ['title', 'pub_date', 'image_url','content']
+        widgets ={
+        'pub_date': forms.DateInput(format=('%m/%d/%Y'),
+        attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
+        }
--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+# Added by Karthika V
+from django.urls import reverse
 
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+        )
     pub_date = models.DateTimeField()
     content = models.TextField()
+    # Added by Karthika V
+    image_url = models.URLField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('news:story', kwargs={'pk':self.pk})
